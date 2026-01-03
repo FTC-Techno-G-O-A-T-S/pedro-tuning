c772282f09cdda54aa43732197a318482afa1b24
Techno-Goats-9224 <ftcgoats9224@gmail.com>
2026-01-03 14:40:52 -0600

    finish tuning
    tuned 1/3/26

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -1,5 +1,7 @@
 package org.firstinspires.ftc.teamcode.pedroPathing;
 
+import com.pedropathing.control.FilteredPIDFCoefficients;
+import com.pedropathing.control.PIDFCoefficients;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.follower.FollowerConstants;
 import com.pedropathing.ftc.FollowerBuilder;
@@ -14,10 +16,18 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 
 public class Constants {
     public static FollowerConstants followerConstants = new FollowerConstants()
-            .mass(12.7);
+            .mass(12.7)
+            .forwardZeroPowerAcceleration(-26.77900647735631)//divide ZPAM by 4
+            .lateralZeroPowerAcceleration(-54.321782078788345)
+            .translationalPIDFCoefficients(new PIDFCoefficients(.1,0,0,.03))
+            .headingPIDFCoefficients(new PIDFCoefficients(.8,0,0.01,.025))
+            .drivePIDFCoefficients(new FilteredPIDFCoefficients(0.025,0,0.00001,0.6, 0.01))
+            .centripetalScaling(0.0003);
 
     public static MecanumConstants driveConstants = new MecanumConstants()
             .maxPower(1)
+            .xVelocity(70.84822083065745)
+            .yVelocity(51.99754412257096)
             .rightFrontMotorName("fr")
             .rightRearMotorName("br")
             .leftRearMotorName("bl")
@@ -28,18 +38,18 @@ public class Constants {
             .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD);
 
     public static ThreeWheelIMUConstants localizerConstants = new ThreeWheelIMUConstants()
-            .forwardTicksToInches(.001989436789)
-            .strafeTicksToInches(.001989436789)
-            .turnTicksToInches(.001989436789)
+            .forwardTicksToInches(.001958346318235686) //start .001989436789
+            .strafeTicksToInches(.0019256211129543691) // start .001989436789
+            .turnTicksToInches(.0020434095631943292)
             .leftPodY(4.875)
             .rightPodY(-4.875)
             .strafePodX(-4)
             .leftEncoder_HardwareMapName("fl")
             .rightEncoder_HardwareMapName("br")
-            .strafeEncoder_HardwareMapName("fr")
+            .strafeEncoder_HardwareMapName("bl")
             .leftEncoderDirection(Encoder.FORWARD)
             .rightEncoderDirection(Encoder.FORWARD)
-            .strafeEncoderDirection(Encoder.FORWARD)
+            .strafeEncoderDirection(Encoder.REVERSE)
             .IMU_HardwareMapName("imu")
             .IMU_Orientation(new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.LEFT, RevHubOrientationOnRobot.UsbFacingDirection.BACKWARD));
     public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
